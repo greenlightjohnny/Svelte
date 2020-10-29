75ceab9cf321f7d235a87c21a09eb54a0cc2e0e0
fj <castelloiv@gmail.com>
2020-10-29 13:14:36 -0400

    Practice

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,52 @@
 <script>
-	export let name;
+  import { text } from "svelte/internal";
+
+  export let firstName = "Sam";
+  let lastName = "Hendrix";
+  let beltColor = "black";
+
+  $: fullName = `${firstName} ${lastName}`;
+  $: {
+    console.log(beltColor);
+    console.log(fullName);
+  }
+
+  const handleClick = () => {
+    beltColor = "orange";
+  };
+
+  const handleInput = (e) => {
+    beltColor = e.target.value;
+  };
+
+  let people = [{ name: "Kim" }];
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <p>{fullName} - belt</p>
+  <input type="text" bind:value={firstName} />
+  <input type="text" bind:value={lastName} />
+  <input type="text" bind:value={beltColor} />
+</main>
